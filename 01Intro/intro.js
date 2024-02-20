bcef3017e9eb95f3b7cd669b91a8129e0c72c994
sanjeet kumar <skc0845@gmail.com>
2024-02-20 23:46:27 +0530

    intro:typescript

---
+++ 01Intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Sanjeeet", city: "Delhi" };
+console.log("Hello Typescript");
+console.log("I am ".concat(user.name, " from ").concat(user.city));
---
+++ 01Intro/intro.ts
@@ -0,0 +1,4 @@
+const user = { name: "Sanjeeet", city: "Delhi" };
+console.log("Hello Typescript");
+
+console.log(`I am ${user.name} from ${user.city}`);
